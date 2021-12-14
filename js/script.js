d0ea602b168ff5752ee5bcd7830986bfb36355de
Javed Mistari <67035126+javedmistari@users.noreply.github.com>
2021-12-14 13:56:34 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,71 +1,70 @@
 $(document).ready(function () {
 
- 
-        // JQUERY NAV TOGGLE
-        $('#menu').click(function (){
-            $('.navBar').slideToggle();
-        });
-  
-        $(window).resize(function(){  
-            var w = $(window).width();  
-            if(w > 768) {  
-                $('.navBar').removeAttr('style');  
-            }  
-        });
+// MOBILE MENU
+    $('#menu').click(function () {
+        $('.navBar').slideToggle();
+    });
 
-      
-        
+    $(window).resize(function () {
+        var w = $(window).width();
+        if (w > 768) {
+            $('.navBar').removeAttr('style');
+        }
+    });
 
-            $("#actionForm").validate({
-                rules: {
-                  name : {
-                    required: true,
-                    minlength: 3
-                  },
-                  lastName : {
-                    required: true,
-                    minlength: 3
-                  },
-                  email: {
-                    required: true,
-                    email: true
-                  },
-                  mobile: {
-                    required: true,
-                    number: true,
-                    minlength: 10,
-                    maxlength: 10
-                  },
-                  city : {
-                    required: true,
-                    minlength: 3
-                  },
-                  state : {
-                    required: true,
-                    minlength: 3
-                  },
-                  tick : {
-                    required: true,
 
-                  }
-                },
-           
-              });
-      
-          
-  
 
+// FORM VALIDATION
+    $("#actionForm").validate({
+        rules: {
+            name: {
+                required: true,
+                minlength: 3
+            },
+            lastName: {
+                required: true,
+                minlength: 3
+            },
+            email: {
+                required: true,
+                email: true
+            },
+            mobile: {
+                required: true,
+                number: true,
+                minlength: 10,
+                maxlength: 10
+            },
+            city: {
+                required: true,
+                minlength: 3
+            },
+            state: {
+                required: true,
+                minlength: 3
+            },
+            tick: {
+                required: true,
+
+            }
+        },
+
+    });
+
+
+
+// ACCORDION
 
     $(".faqHeading").click(function () {
-        if ($(".faqDescription").is(":visible")){
+        if ($(".faqDescription").is(":visible")) {
             $(".faqDescription").slideUp(300);
             $(".plusminus").text('+');
         }
 
-        if($(this).next(".faqDescription").is(":visible")){
+        if ($(this).next(".faqDescription").is(":visible")) {
             $(this).next(".faqDescription").slideUp(300);
             $(this).children(".plusminus").text('+');
-        }else{
+        } else {
             $(this).next(".faqDescription").slideDown(300);
             $(this).children(".plusminus").text('-');
         }
